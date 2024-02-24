cd6f58f70c3722d7cacd61d9486e2d97ac7100c7
lunanarae <lunanarae@gmail.com>
2024-02-24 12:11:46 +0800

    add javascript to get actual data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function currentWeatherDetails(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "033b43a04493c2b0f53e8fe8bdote92d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentWeatherDetails);
+}
+
 function search(event) {
   event.preventDefault();
   let searchFormInputElement = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchFormInputElement.value;
+
+  searchCity(searchFormInputElement.value);
 }
 let searachFormElement = document.querySelector("#search-form");
 searachFormElement.addEventListener("submit", search);
+
+searchCity("Perth");
